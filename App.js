f6a573af55d50f68db45a035da2b494c89d290c2
Youssef El habchi <ellhydra1@gmail.com>
2020-11-28 18:40:45 +0100

    Initial commit

--- App.js
+++ App.js
@@ -1,21 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { ScrollView, Dimensions, Linking } from 'react-native';
+import HTML from 'react-native-render-html';
+ 
+const htmlContent = `
+    <h1>VR Lorem ipsum dolor sit amet, consectetur adipiscing elit. !</h1>
+    <em>By <b class="author">React Native Master</b></em>
+    <img src="https://image.freepik.com/free-photo/young-woman-using-vr-glasses-with-neon-lights_155003-17747.jpg" />
+    <p>Vivamus bibendum feugiat pretium. <a href="https://reactnativemaster.com/">Vestibulum ultricies rutrum ornare</a>. Donec eget suscipit tortor. Nullam pellentesque nibh sagittis, pharetra quam a, varius sapien. Pellentesque ut leo id mauris hendrerit ultrices et non mauris. Quisque gravida erat at felis tincidunt tincidunt. Etiam sit amet egestas leo. Cras mollis mi sed lorem finibus, interdum molestie magna mollis. Sed venenatis lorem nec magna convallis iaculis.</p>
+    <iframe height="315" src="https://www.youtube.com/embed/fnCmUWqKo6g" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+`;
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+ 
+export default class App extends Component {
+  render () {
+      return (
+          <ScrollView style={{ flex: 1, paddingTop:50, paddingHorizontal: 20 }}>
+              <HTML 
+                html={htmlContent} 
+                imagesMaxWidth={Dimensions.get('window').width * .9 } 
+                staticContentMaxWidth={Dimensions.get('window').width * .9 }
+                tagsStyles={tagsStyles}
+                classesStyles={classesStyles}
+                onLinkPress={(event, url) =>  Linking.openURL(url)}
+                />
+          </ScrollView>
+      );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const classesStyles = {
+  'author': {
+    color: '#CA43AC',
   },
-});
+}
+
+const tagsStyles = {
+  h1: {
+    color: '#6728C7',
+    textAlign: 'center',
+    marginBottom: 10
+  },
+  img: {
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    marginTop: 20
+  }
+}
